55f125faa93d8ec5322de6a81c7a726f86139232
Oscar <oscar_ochoac@yahoo.com>
2019-06-30 05:01:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,6 +11,7 @@ def monopoly_with_second_tier
 	    :pieces => 4,
 	    :names => {},
 	    :rent_in_dollars => {
+	      :one_piece_owned => 25
 	    }
 	  }
 	}
@@ -19,6 +20,8 @@ end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	
+	
 
 end
 
